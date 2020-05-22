fdfe55d19262587a327d54a7eaeae985d252cc80
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-05-22 15:36:40 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(100)
+            led.unplot(x, y)
+            basic.pause(100)
+        }
+    }
+})
